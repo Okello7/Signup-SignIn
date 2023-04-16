7ddb06220591d0a4dcba1687065a0eca580b78ce
itsOkello <37811656+Okello7@users.noreply.github.com>
2023-04-16 21:52:53 +0300

    Add files via upload

---
+++ SignUp-SignIn/student-registration-form/classic-login-form/script.js
@@ -0,0 +1,6 @@
+var buttonLogin = document.getElementById('do-login');
+var idLogin = document.getElementById('login');
+var username = document.getElementById('username');
+buttonLogin.onclick = function(){
+  idLogin.innerHTML = '<p>We\'re happy to see you again, </p><h1>' +username.value+ '</h1>';
+}
